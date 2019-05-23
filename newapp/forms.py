0251fdf84e1dc27458aeb3c5503bfec447fc2bca
root <apjanco@gmail.com>
2019-05-23 17:20:26 +0000

    pushing changes from main copy

--- newapp/forms.py
+++ newapp/forms.py
@@ -4,3 +4,6 @@ from newapp.models import Course
 class CourseForm(forms.Form):
     class Meta:
         model = Course
+
+class SearchForm(forms.Form):
+	search = forms.CharField(label='search', max_length=100)
