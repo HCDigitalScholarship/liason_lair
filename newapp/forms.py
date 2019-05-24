aa611704f3d0d7f0ded6d1e3d49c8f23def3765d
noooorfatima <50842914+noooorfatima@users.noreply.github.com>
2019-05-24 14:42:56 -0400

    Update forms.py

--- newapp/forms.py
+++ newapp/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from newapp.models import Course, Category, Question
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Submit
+from crispy_forms.layout import Layout, Submit, ButtonHolder
 
 class CourseForm(forms.Form):
     class Meta:
@@ -21,7 +21,7 @@ class ContactForm(forms.Form):
         super().__init__(*args,**kwargs)
         self.helper = FormHelper
         self.helper.form_method = 'post'
-        self.helper.layout = Layout('name', 'email', 'category', 'subject', 'body', Submit('submit','Submit', css_class="button green"))
+        self.helper.layout = Layout('name', 'email', 'category', 'subject', 'body', ButtonHolder(Submit('submit','Submit', css_class="button green")))
         #css class portion does not seem to work
 
 # want to be talking to Questions model, dunno if we can go crispy on this one
